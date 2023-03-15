881bb66475155d637e7244488458c287073e89c3
unknown <rknowles12301@gmail.com>
2023-03-15 09:23:01 -0700

    Copy and pasted over answers in repository.

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -11,30 +11,40 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
-
+Select * From movies Left Join users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
+Select * From movies Inner Join users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
+Select lead_studio,count(*) as occurrences From movies Inner Join users on movie_id = favorite_movie_id
+ Group by lead_studio
+ Order by count(*)
+ Desc
+ limit 1;
+
+
 
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
 
+Select avg(rotten_tomatoes) From movies Join users on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
-
+Select film,count(*) as duplicates from movies 
+Group by film
+Having count(film) > 1
